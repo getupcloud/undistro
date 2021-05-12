64160ddacf4fe11805df0f0f5c4646445bac6f0b
Felipe Oliveira <fpo@felipeweb.dev>
2021-05-12 17:30:29 -0300

    fix e2e
    Signed-off-by: Felipe Oliveira <fpo@felipeweb.dev>

--- e2e/install_test.go
+++ e2e/install_test.go
@@ -32,34 +32,34 @@ var _ = Describe("Validate UnDistro Installation", func() {
 			}
 			return running
 		}, 10*time.Minute, 1*time.Minute).Should(BeTrue())
-		It("Verify if UnDistro AWS is correctly installed", func() {
-			Eventually(func() string {
-				s := corev1.Secret{}
-				key := client.ObjectKey{
-					Name:      "undistro-aws-config",
-					Namespace: "undistro-system",
-				}
-				err := k8sClient.Get(context.Background(), key, &s)
-				Expect(err).ToNot(HaveOccurred())
-				return string(s.Data["credentials"])
-			}, 10*time.Minute, 1*time.Minute).ShouldNot(BeEmpty())
-		})
-		It("Check tested image", func() {
-			sha := os.Getenv("GITHUB_SHA")
-			image := fmt.Sprintf("localhost:5000/undistro:%s", sha)
-			Eventually(func() string {
-				podList := corev1.PodList{}
-				err := k8sClient.List(context.Background(), &podList, client.InNamespace("undistro-system"))
-				Expect(err).ToNot(HaveOccurred())
-				for _, p := range podList.Items {
-					for _, container := range p.Spec.Containers {
-						if container.Image == image {
-							return container.Image
-						}
+	})
+	It("Verify if UnDistro AWS is correctly installed", func() {
+		Eventually(func() string {
+			s := corev1.Secret{}
+			key := client.ObjectKey{
+				Name:      "undistro-aws-config",
+				Namespace: "undistro-system",
+			}
+			err := k8sClient.Get(context.Background(), key, &s)
+			Expect(err).ToNot(HaveOccurred())
+			return string(s.Data["credentials"])
+		}, 10*time.Minute, 1*time.Minute).ShouldNot(BeEmpty())
+	})
+	It("Check tested image", func() {
+		sha := os.Getenv("GITHUB_SHA")
+		image := fmt.Sprintf("localhost:5000/undistro:%s", sha)
+		Eventually(func() string {
+			podList := corev1.PodList{}
+			err := k8sClient.List(context.Background(), &podList, client.InNamespace("undistro-system"))
+			Expect(err).ToNot(HaveOccurred())
+			for _, p := range podList.Items {
+				for _, container := range p.Spec.Containers {
+					if container.Image == image {
+						return container.Image
 					}
 				}
-				return ""
-			}, 10*time.Minute, 1*time.Minute).Should(Equal(image))
-		})
+			}
+			return ""
+		}, 10*time.Minute, 1*time.Minute).Should(Equal(image))
 	})
 })
