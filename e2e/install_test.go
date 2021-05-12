63d8db2f140b9f7cf7cbf7c6d9f5f3ea603f757c
Felipe Oliveira <fpo@felipeweb.dev>
2021-05-12 16:55:23 -0300

    more install tests
    Signed-off-by: Felipe Oliveira <fpo@felipeweb.dev>

--- e2e/install_test.go
+++ e2e/install_test.go
@@ -2,6 +2,8 @@ package e2e_test
 
 import (
 	"context"
+	"fmt"
+	"os"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -41,6 +43,23 @@ var _ = Describe("Validate UnDistro Installation", func() {
 				Expect(err).ToNot(HaveOccurred())
 				return string(s.Data["credentials"])
 			}, 10*time.Minute, 1*time.Minute).ShouldNot(BeEmpty())
+		})
+		It("Check tested image", func() {
+			sha := os.Getenv("GITHUB_SHA")
+			image := fmt.Sprintf("localhost:5000/undistro:%s", sha)
+			Eventually(func() string {
+				podList := corev1.PodList{}
+				err := k8sClient.List(context.Background(), &podList, client.InNamespace("undistro-system"))
+				Expect(err).ToNot(HaveOccurred())
+				for _, p := range podList.Items {
+					for _, container := range p.Spec.Containers {
+						if container.Image == image {
+							return container.Image
+						}
+					}
+				}
+				return ""
+			}, 10*time.Minute, 1*time.Minute).Should(Equal(image))
 		})
 	})
 })
