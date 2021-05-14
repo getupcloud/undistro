6e715bae355961d636bf94056ec3ae4fed11d0aa
Felipe Oliveira <fpo@felipeweb.dev>
2021-05-14 16:44:39 -0300

    debug
    Signed-off-by: Felipe Oliveira <fpo@felipeweb.dev>

--- e2e/install_test.go
+++ e2e/install_test.go
@@ -91,6 +91,13 @@ var _ = Describe("Validate UnDistro Installation", func() {
 			klog.Info(err)
 			klog.Info(string(out))
 			cmd = exec.NewCommand(
+				exec.WithCommand("undistro"),
+				exec.WithArgs("get", "pods", "-n", "undistro-system"),
+			)
+			out, _, err = cmd.Run(context.Background())
+			klog.Info(err)
+			klog.Info(string(out))
+			cmd = exec.NewCommand(
 				exec.WithCommand("helm"),
 				exec.WithArgs("ls", "-n", "undistro-system"),
 			)
